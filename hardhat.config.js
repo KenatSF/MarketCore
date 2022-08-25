bb3712afd870433ec05c2b334ca53fd06eb887f6
KenatSF <banerabh@gmail.com>
2022-08-24 22:42:33 -0500

    Set up basics

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,56 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+require('hardhat-contract-sizer');
+require("dotenv").config();
+const PRIVATE_KEY_0 = process.env.PRIVATE_KEY_0 || "";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: {
+    compilers: [
+        {
+            version: "0.8.0"
+        },
+        {
+            version: "0.8.1"
+        }
+    ]
+  },
+  networks: {
+    hardhat:{
+      chainId: 1337
+    }, 
+    avalanche: {
+      url: `https://api.avax.network/ext/bc/C/rpc`,
+      chainId: 43114,
+      accounts: [PRIVATE_KEY_0],
+      gasLimit: 3000000,
+      gasPrice: 27000000000
+    },
+    fuji: {
+      url: `https://api.avax-test.network/ext/bc/C/rpc`,
+      chainId: 43113,
+      accounts: [PRIVATE_KEY_0],
+      gasLimit: 3000000,
+      gasPrice: 27000000000
+    },
+    polygon: {
+      url: `https://rpc-mainnet.maticvigil.com/`,
+      chainId: 137,
+      accounts: [PRIVATE_KEY_0],
+      gasLimit: 3000000,
+      gasPrice: 80000000000
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY_AVAX_SCAN
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
 };

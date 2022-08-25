5446f0d632e06083580b8051e3c7a1b49c0b812f
KenatSF <banerabh@gmail.com>
2022-08-24 23:15:10 -0500

    Kronos token added

--- contracts/Kronos.sol
+++ contracts/Kronos.sol
@@ -1,7 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { AccessControl } from  '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract Kronos {
+contract Kronos is ERC20, Ownable, AccessControl {
+
+    uint256 private _maxSupply = 100000000*1e18;
+
+    bytes32 public constant ROL_MINTER = keccak256("ROL_MINTER");
+
+    constructor() ERC20("Kronos", "KRN") {}
+
+    modifier onlyMinter() {
+        require(hasRole(ROL_MINTER, msg.sender), "Only a minter can execute this function!");
+        _;
+    }
+
+    function maxSupply() public view virtual returns (uint256) {
+        return _maxSupply;
+    }
+
+    function addMinter(address account) public virtual onlyOwner {
+        _grantRole(ROL_MINTER,account);
+    }
+
+    function removeMinter(address account) public virtual onlyOwner {
+        _revokeRole(ROL_MINTER, account);
+    }
+
+    function mint(address account, uint256 amount) public virtual onlyMinter {
+        require((totalSupply() + amount) <= maxSupply(), "Max Supply reached!");
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public virtual onlyMinter {
+        _burn(msg.sender, amount);
+    }
     
 }

10875f003e960b475f7d4259745381b02919feed
KenatSF <banerabh@gmail.com>
2022-09-05 18:03:15 -0500

    Kronos smart contract, test completed!

---
+++ test/Kronos.test.js
@@ -0,0 +1,72 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const maxAmount = "500";
+    // Accounts managament
+    const [user0, user1, user2, user3, deployer ] = await ethers.getSigners();
+
+
+    // Deploy contracts
+    const KRN = await hre.ethers.getContractFactory("Kronos");
+    const krn = await KRN.connect(deployer).deploy();
+    await krn.deployed();
+    const kronosAddress = krn.address;
+
+    console.log('-----------------------------------------------------------');
+    console.log("First call");
+    const owner = await krn.owner();
+    var supply = await krn.totalSupply();
+
+    console.log('-----------------------------------------------------------');
+    console.log("Show first call!");
+    console.log(`Kronos address: ${kronosAddress}`);
+    expect(deployer.address).to.equal(owner);
+    expect(supply).to.equal(0);
+
+    console.log('-----------------------------------------------------------');
+    console.log("Second call");
+    await krn.connect(deployer).mint(deployer.address, ethers.utils.parseUnits("250", "ether"));
+    supply = await krn.totalSupply();
+
+
+    console.log('-----------------------------------------------------------');
+    console.log("Show second call!");
+    expect(supply).to.equal(ethers.utils.parseUnits("250", "ether"));
+
+    
+    console.log('-----------------------------------------------------------');
+    console.log("Third call");
+    try {
+        await krn.connect(user0).mint(user0.address, ethers.utils.parseUnits("250", "ether"));
+    } catch(e) {
+        console.log("Error");
+    }
+    supply = await krn.totalSupply();
+
+    console.log('-----------------------------------------------------------');
+    console.log("Show third call!");
+    expect(supply).to.equal(ethers.utils.parseUnits("250", "ether"));
+
+
+    console.log('-----------------------------------------------------------');
+    console.log("Fourth call!");
+    await krn.connect(deployer).mint(deployer.address, ethers.utils.parseUnits("250", "ether"));
+    supply = await krn.totalSupply();
+
+    console.log('-----------------------------------------------------------');
+    console.log("Show fourth call!");
+    expect(supply).to.equal(ethers.utils.parseUnits(maxAmount, "ether"));
+
+
+    console.log('-----------------------------------------------------------');
+    console.log("Fifth call");
+    try {
+        await krn.connect(deployer).mint(deployer.address, ethers.utils.parseUnits("1", "ether"));
+    } catch(e) {
+        console.log("Error");
+    }
+
+  });
+});
